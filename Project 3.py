98018348d4fc330822f6b2d183438599b6b5a3e8
ThatBenKraft <ben.kraft@rcn.com>
2023-02-08 21:58:51 -0500

    Cleanup

--- Project 3.py
+++ Project 3.py
@@ -1,9 +1,9 @@
 import time
 
-import motor_driver
-from motor_driver import Directions, Sequences
+import gpio_driver
+from gpio_driver import Directions, Sequences
 
 time.sleep(1)
-motor_driver.pin_setup()
-motor_driver.step(50, Sequences.HALFSTEP, Directions.COUNTER_CLOCKWISE)
-motor_driver.pin_cleanup()
+gpio_driver.pin_setup()
+gpio_driver.step(200, Sequences.HALFSTEP, Directions.COUNTER_CLOCKWISE)
+gpio_driver.pin_cleanup()
